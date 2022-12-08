13511dc449d9473e0ab3fe8c023e7a43382be392
msaybek <mchdaybkk@gmail.com>
2022-12-08 11:12:11 +0300

    minor changes

--- manager.cpp
+++ manager.cpp
@@ -4,11 +4,7 @@
 #include <QAbstractItemModel>
 #include <QDialog>
 
-QString GetRandomString(int);
-int encrypt(unsigned char*, int, unsigned char*, unsigned char*);
-int decrypt(unsigned char*, int, unsigned char*, unsigned char*);
-unsigned char* StringToChar(QString);
-QString getStringFromUnsignedChar(unsigned char[64],int);
+
 int deneme();
 bool letter_special_number(QString);
 bool letter_special(QString);
@@ -46,54 +42,6 @@ manager::manager(QWidget *parent) :
 
     connect(ui->tableWidget, &QTableWidget::cellClicked, this, &manager::cellClicked);
 }
-
-
-
-class EncryptDecrypt
-{
-public:
-    unsigned char* key = (unsigned char*)"0123456789abcdef";      //disaridan olusturup veriyoruz..sifreleme icin anahtar
-    int cipher_len = 0;
-    unsigned char cipher[64];
-
-    QString encrypt_it(QString password)
-    {
-        QString encrypted_text;
-        unsigned char* text = (unsigned char*)StringToChar(password);
-        int text_len = strlen((const char*)text);
-        memset(&cipher[0], 0, sizeof(cipher));  //cipher sıfırlanıyor ki, temiz bir sekilde sifreleyelim..
-        cipher_len = encrypt(text, text_len, key, cipher);  //CİPHER 64'LUK BİR CHAR ARRAY
-        free(text);
-        encrypted_text = getStringFromUnsignedChar(cipher, cipher_len);
-        return encrypted_text;  //SIFRENIN HEX'LI HALI
-    }
-//encrypt_it ile birlikte girilen parola 64'luk bir char array oluyor kutuphane ile,
-//biz ise onu hex'e donusturup db'e aktariyoruz..db'de o sekilde bekliyor..sonra ise
-//db'den hex'li hali aliniyor 64'luk char array haline tekrar getiriliyor cunku
-//kutuphane fonk'u ancak bu sekilde duzgun calisabiliyor, daha sonra bu 64'luk char
-//array ilgili fonk'a gonderilip asil girilen parola qstring ile tekrar normal hale
-//getirilip ilgili yerde ekrana bastiriliyor..
-//SON DURUMDA BU CLASS FONK'LARI DUZGUN CALISMAKTADIR..
-    QString decrypt_it(QString crypt_password, int cipher_len)
-    {
-        QString decrypted_string;
-        unsigned char decrypted[64];    //yeni 64'luk char array, duz parola..
-        unsigned char cipher[64];
-        for(int i=0;i<cipher_len;++i)   //normalde 32
-        {
-             QString hexString = crypt_password.mid(i*2,2);
-             bool ok = false;
-             cipher[i] = (unsigned char)hexString.toUShort(&ok,16);  //if not ok, handle error
-        }
-        //cipher 64'luk bir char array, password'un donusturulmus hali..
-        int dec_len = decrypt(cipher, cipher_len, key, decrypted);
-        for(int i=0; i<dec_len; i++)
-        {
-            decrypted_string += decrypted[i];       //EKK
-        }
-        return decrypted_string;    //cozulmus string bunun icinde...
-    }
-};
 
 
 void manager::cellClicked()
